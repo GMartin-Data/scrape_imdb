21818c3c4486ddae91bb50cf8e4ef71525eb1ff3
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-15 14:13:11 +0100

    Adapt URL to scrape 250 best films

--- imdbscraper/imdbscraper/spiders/filmspider.py
+++ imdbscraper/imdbscraper/spiders/filmspider.py
@@ -4,7 +4,7 @@ import scrapy
 class FilmspiderSpider(scrapy.Spider):
     name = "filmspider"
     allowed_domains = ["imdb.com"]
-    start_urls = ["https://imdb.com"]
+    start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
 
     def parse(self, response):
         pass
