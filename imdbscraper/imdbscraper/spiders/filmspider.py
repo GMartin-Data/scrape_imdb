1d0e66cbb99db8c4a173626c93656354da797229
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-18 14:15:54 +0100

    Fix problem with minutes designed with 'm'

--- imdbscraper/imdbscraper/spiders/filmspider.py
+++ imdbscraper/imdbscraper/spiders/filmspider.py
@@ -6,7 +6,7 @@ from imdbscraper.items import FilmItem
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-    "Accept-Language": "en"
+    "Accept-Language": "en-US,en;q=0.9"
 }
 
 class FilmspiderSpider(scrapy.Spider):
