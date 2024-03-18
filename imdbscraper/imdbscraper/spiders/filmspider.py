513dcac524ea1b06d30e93f9646b3bd27132cf4d
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-18 13:59:45 +0100

    Switched to english

--- imdbscraper/imdbscraper/spiders/filmspider.py
+++ imdbscraper/imdbscraper/spiders/filmspider.py
@@ -6,7 +6,7 @@ from imdbscraper.items import FilmItem
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-    "Accept-Language": "fr"
+    "Accept-Language": "en"
 }
 
 class FilmspiderSpider(scrapy.Spider):
