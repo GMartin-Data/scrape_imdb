0a675748c7b9b1f130534d4539ae8c3290abb102
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-18 11:23:45 +0100

    Add pipeline for storage in sqlite3 database

--- imdbscraper/imdbscraper/pipelines.py
+++ imdbscraper/imdbscraper/pipelines.py
@@ -5,9 +5,56 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
+
 from itemadapter import ItemAdapter
 
 
-class ImdbscraperPipeline:
+class FilmscraperPipeline:
+    def __init__(self):
+        self.con = sqlite3.connect("imdb.db")
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute("""
+                         CREATE TABLE IF NOT EXISTS films(
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            title TEXT,
+                            original_title TEXT,
+                            score REAL,
+                            year INTEGER,
+                            audience TEXT,
+                            duration INTEGER,
+                            genres TEXT,
+                            synopsis TEXT,
+                            main_casting TEXT,
+                            countries TEXT
+                         )
+                         """)
+        
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        self.cur.execute(
+            """INSERT INTO films (
+            title, original_title, score, year, audience, duration, genres, synopsis, main_casting, countries
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            (
+                adapter.get("title"),
+                adapter.get("original_title"),
+                adapter.get("score"),
+                adapter.get("year"),
+                adapter.get("audience"),
+                adapter.get("duration"),
+                adapter.get("genres"),
+                adapter.get("synopsis"),
+                adapter.get("main_casting"),
+                adapter.get("countries")
+            )
+        )
+        self.con.commit()
         return item
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.con.close()
--- imdbscraper/imdbscraper/settings.py
+++ imdbscraper/imdbscraper/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "imdbscraper.pipelines.ImdbscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "imdbscraper.pipelines.FilmscraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
